f8088f1b8371eec90fed844ca980541fb07d281d
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2018-08-13 15:20:58 +0530

    Update Test.java

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -38,10 +38,10 @@ public class Test{
 		Connection cx = null;
 		MysqlDataSource dataSource = new MysqlDataSource();
 		dataSource.setUser("root");
-		dataSource.setPassword("root");
-		dataSource.setServerName("localhost");
+		dataSource.setPassword("pass@123");
+		dataSource.setServerName("20.0.3.9");
 		dataSource.setPortNumber(3306);
-		dataSource.setDatabaseName("competencydevops");
+		dataSource.setDatabaseName("client_db1");
 		 cx = dataSource.getConnection();
 		 System.out.println("CONNECTION SUCCESSFULL");
 		
