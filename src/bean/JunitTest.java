f61667d2de465a6c54c4d2f5bf28b60220b4202d
Code-Kartikey12 <38080424+Code-Kartikey12@users.noreply.github.com>
2018-08-17 17:16:30 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -26,7 +26,7 @@ Assert.assertEquals(Actualtitle, Expectedtitle);
 //print out the result
 System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
        
-	}
+	
 	
 		
 }
