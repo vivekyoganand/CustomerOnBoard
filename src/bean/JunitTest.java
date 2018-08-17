2d7b9ff5b502b4f2b57781ef2a21d8e24773e242
Code-Kartikey12 <38080424+Code-Kartikey12@users.noreply.github.com>
2018-08-17 17:20:14 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -14,17 +14,17 @@ public class JunitTest {
 		
 	
 	WebDriver driver = newFirefoxDriver();
-//it will open the goggle page
-driver.get("http://google.in"); 
-//we expect the title “Google “ should be present 
-String Expectedtitle = "Google";
-//it will fetch the actual title 
-String Actualtitle = driver.getTitle();
-System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
-//it will compare actual title and expected title
-Assert.assertEquals(Actualtitle, Expectedtitle);
-//print out the result
-System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+	//it will open the goggle page
+	driver.get("http://www.google.com"); 
+	//we expect the title “Google “ should be present 
+	String Expectedtitle = "Google";
+	//it will fetch the actual title 
+	String Actualtitle = driver.getTitle();
+	System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+	//it will compare actual title and expected title
+	Assert.assertEquals(Actualtitle, Expectedtitle);
+	//print out the result
+	System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
        
 	
 	
