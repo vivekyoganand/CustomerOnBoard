f04e43dcbfd9552fe39b641927b2dd574f754ef7
Code-Kartikey12 <38080424+Code-Kartikey12@users.noreply.github.com>
2018-08-20 17:49:01 +0530

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -9,7 +9,7 @@ public class JunitTest {
 	@Test
 	public void login() {
 		//System.out.println("Logging into my account");
-		System.setProperty("webdriver.gecko.driver", "~/firefox/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "/var/lib/jenkins/workspace/testkar/geckodriver");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com/");
 		String expectedTitle="Google";
